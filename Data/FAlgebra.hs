2eb2f79264c2fb522c09159fc1a1a16e4602eb06
Brian Hamrick <brian.c.hamrick@gmail.com>
2015-01-06 20:30:11 -0800

    Cabal file for examples

---
+++ Data/FAlgebra.hs
@@ -0,0 +1,9 @@
+module Data.FAlgebra
+    ( module Data.FAlgebra.Annotation
+    , module Data.FAlgebra.Base
+    , module Data.FAlgebra.Hole
+    ) where
+
+import Data.FAlgebra.Annotation
+import Data.FAlgebra.Base
+import Data.FAlgebra.Hole
